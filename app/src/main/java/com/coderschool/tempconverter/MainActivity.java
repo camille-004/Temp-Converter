36a843364df907b393001f7446bc1bd6683e93d8
Camille Dunning <camilledunning@Camilles-MacBook-Pro.local>
2017-02-20 20:17:56 -0800

    version 1.0 commit

--- app/src/main/java/com/coderschool/tempconverter/MainActivity.java
+++ app/src/main/java/com/coderschool/tempconverter/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText editView;
     private ToggleButton button;
 
+    private final String c = "celsius";
+    private final String f = "fahrenheit";
+
     private void bindUI() {
         textView = (TextView) findViewById(R.id.textView);
         editView = (EditText) findViewById(editText);
@@ -33,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private void addEventListeners() {
         addEditViewListeners();
         addToggleButtonListeners();
+
     }
 
     private void addToggleButtonListeners() {
@@ -40,7 +45,14 @@ public class MainActivity extends AppCompatActivity {
         button.setTextOn("Fahrenheit --> Celsius");
         button.setTextOff("Celsius --> Fahrenheit");
 
-        //TODO: Add event listener to recalcuate conversion.
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                convertTemperature();
+            }
+
+        });
     }
 
     private void addEditViewListeners() {
@@ -58,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         String s = editView.getText().toString();
         if (s.length() == 0) {
             textView.setText(setString(convert(0.0)));
-            editView.setHint("0 degrees celsius");
+            editView.setHint("0 degrees " + (button.isChecked() ? f : c));
             return;
         }
         Double input = Double.parseDouble(s.toString());
@@ -68,14 +80,10 @@ public class MainActivity extends AppCompatActivity {
 
     private String setString(Double input) {
         //%.1f restricts number to 1 decimal point
-        return String.format("%.1f degrees fahrenheit", input);
+        return String.format("%.1f degrees " + (button.isChecked() ? f : c), input);
     }
 
     private Double convert(Double input) {
-        if (!button.isChecked()) {
-            return (1.8 * input) + 32;
-        } else {
-            return (input - 32) * (5 / 9);
-        }
+        return (button.isChecked() ? (input - 32) * (5.0 / 9.0) : (1.8 * input) + 32 );
     }
 }
